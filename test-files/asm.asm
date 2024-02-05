6883bfa48a226c864e418b3e1d18f503a03c4b49
jrunge <jrunge@precisionplanting.com>
2024-02-04 23:22:49 -0600

    continued play

--- test-files/asm.asm
+++ test-files/asm.asm
@@ -1,17 +1,34 @@
 SECTION .data
-msg     db      'Hello World!', 0Ah
+msg     db      'Hello World!'
 
 SECTION .text
 global _start
 
 _start:
+	mov		edx, msg
+	call	strlen
 
-	mov	eax, 4
-	mov	ebx, 1
-	mov	ecx, msg
-	mov	edx, 13
-	int	0x80
+	; mov		edx, eax
+	mov		ecx, msg
+	mov		ebx, 1
+	mov		eax, 4
+	int		0x80
 
-	mov	eax, 1
-	mov	ebx, 0
-	int	0x80
+	mov		eax, 1
+	mov		ebx, 0
+	int		0x80
+
+strlen:
+	push   	rbx				; push ebx onto the stack
+	mov		ebx, edx
+
+nextchar:
+	cmp		byte [edx], 0
+	jz		finished
+	inc		edx
+	jmp		nextchar
+
+finished:
+	sub		edx, ebx
+	pop		rbx				; pop value on the stack back into ebx
+	ret						; return to the calling function
